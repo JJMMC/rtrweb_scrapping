ea63322ed1c4217a46997a384a4c871565cab6fd
JJMMC <jjmmc@hotmail.com>
2024-11-09 09:57:50 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from rtr_scrap_func import *
+
+#### EJECUCIÓN DEL PROGRAMA PPAL
+
+sitemenu = rtr_request_sitemenu("https://www.rtrvalladolid.es/87-crawler")
+    
+    # Imprime en terminal el menu para seleccionar la familia a scrapear
+contador = 1
+for i in sitemenu:
+    print(f"{contador}.-{i}")
+    contador = contador + 1
+seleccionado = int(input("Selecciona lo que quieres Scrapear: "))
+
+    # Llamamos a las funciones para obtener los datos
+url_seleccionada = (seleccion_familia(seleccionado))
+lista_url_seleccionadas = rtr_pages_to_scan(url_seleccionada)
+
+    # Almacenamos datos 
+datos_scrapeados_tot = list()
+for url in lista_url_seleccionadas:
+    datos_scrapeados_pag = rtr_request_multi_data(url)
+    datos_scrapeados_tot = datos_scrapeados_tot + datos_scrapeados_pag
+    # y los presentamos en terminal
+for i in datos_scrapeados_tot:
+    print (i)
+print ("Número de artículos: ", len(datos_scrapeados_tot))
+
